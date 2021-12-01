41bc28a883e2a0ac7360d04901152832e09401c0
arwin <eychpangaila@gmail.com>
2021-12-01 07:08:02 -0300

    Final

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
+
+const About = () => {
+  return(
+    <>
+      <Navigation />
+      <div className="container my-5">
+        <hr></hr>
+          <h1>About this Application</h1>
+              <p>
+              &emsp;Pada saat ini banyak daerah di seluruh dunia yang mengalami perubahan cuaca seketika, beberapa orang yang ingin bepergian mengalami kendala diakibatkan tempat yang ingin mereka tuju mengalami cuaca buruk atau cuaca yang dapat mengganggu aktifitas mereka nantinya. Aplikasi ini dibuat bertujuan untuk dapat membantu orang lain yang ingin mengetahui tempat yang ingin mereka kunjungi sedang mengalami cuaca apa sehingga mereka dapat menentukan apakah harus pergi sekarang atau saat cuaca disana telah membaik. Aplikasi ini dapat menghemat waktu orang lain ketika ingin bepergian sehingga tidak terhalangi oleh cuaca.
+                </p>
+                <p>
+                  Nama : Arwin Eych Toloh Pangaila
+                  <br></br>
+                  NIM : 105011910075
+                    <br></br>
+                  Program Studi : Sistem Informasi
+                </p>
+        </div>
+      <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br> 
+      <Footer />
+    </>
+  );
+}
+
+export default About;
